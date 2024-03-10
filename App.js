9eb45931a0d39864454395e0e27b2ff467ebd664
Saadanxar4766 <153888066+Saadanxar4766@users.noreply.github.com>
2024-03-10 19:53:58 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,57 @@
+// let studesnts = ["Saad","Hammad","Ahmed","Asad","Hassan"];
+// studesnts[studesnts.length] = "Hussain";
+// studesnts[2] = "Ali";
+// studesnts[1] = "Abubakar";
+// console.log(studesnts);
+
+
+// Array push method
+// let studesnts = ["Saad","Usman","Ahmed","Asad","Hassan"];
+// let returnvale = studesnts.push("Umer","Hammad") 
+// console.log(studesnts)
+// console.log(returnvale)
+
+
+//Array pop method
+// let studesnts = ["Saad","Usman","Ahmed","Asad","Hassan"];
+// let returnvale = studesnts.pop();
+// console.log(studesnts);
+// console.log(returnvale);
+
+
+// Array unshift method
+// let studesnts = ["Saad","Usman","Ahmed","Asad","Hassan"];
+// let returnvalue = studesnts.unshift("Hammad");
+// consol.log(studesnts)
+// consol.log(returnvalue)
+
+
+
+// let studesnts = ["Saad","Usman","Ahmed","Asad","Hassan"];
+// let returnvale =  studesnts.shift();
+// consol.log(returvalue)
+// consol.log(studesnts)
+
+// let studesnts = ["Saad","Usman","Ahmed","Asad","Hassan"];
+// let returnvale = studesnts.splice(1,0,"irshad" );
+// consol.log(studesnts);
+// // console.log(returnvale);
+
+
+let students = ["Saad", "Usman", "Ahmed", "Asad", "Hassan"];
+let returnValue = students.splice(1, 0, "Irshad");
+console.log(students);
+ console.log(returnValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
